680486bc0707df03100c65445cc90df5bf48ce01
eranelbaz <eranelbaz97@gmail.com>
2021-08-18 18:35:12 +0300

    up to 5

--- main.tf
+++ main.tf
@@ -1121,7 +1121,7 @@ resource "time_sleep" "sleep_for_routes" {
   # Since aws_route doesn't respect timeout, we can add time before the route creation to help mitigate the problem
   # https://github.com/hashicorp/terraform-provider-aws/issues/20227#issuecomment-900589138
   depends_on = [aws_route_table.private, aws_route_table.public, aws_route_table.database]
-  create_duration = "2m"
+  create_duration = "5m"
   destroy_duration = "5m"
 }
 
